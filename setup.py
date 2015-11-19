1111c1015fd6f74af830239730afb362de15b97c
eduardolucioac <eduardolucioac@gmail.com>
2015-11-19 13:52:42 -0200

    Taref 1198! Ajuste extra no versionamento do LBIndex! By Questor

--- setup.py
+++ setup.py
@@ -14,10 +14,15 @@ requires = [
     'traceback2 == 1.4.0',
     'unittest2 == 1.0.1',
     'urllib3 == 1.10.4'
-    ]
+]
+
+'''
+NOTE: Para versionamento usar "MAJOR.MINOR.REVISION.BUILDNUMBER"! By Questor
+http://programmers.stackexchange.com/questions/24987/what-exactly-is-the-build-number-in-major-minor-buildnumber-revision
+'''
 setup(
     name = "LBIndex",
-    version = "0.1.3a",
+    version = "0.1.1.0",
     author = "Lightbase",
     author_email = "pedro.ricardo@lightbase.com.br",
     url = "https://pypi.python.org/pypi/LBIndex",
